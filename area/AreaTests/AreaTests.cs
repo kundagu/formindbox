edd2221d0e7ad4b8e96f70ff7025bec6b6fe3a4f
Sergey <ohaya_kundagu@mail.ru>
2019-11-21 12:11:47 +0500

    upload

---
+++ area/AreaTests/AreaTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using area;
+
+
+namespace AreaTests
+{
+    public class AreaTest
+    {
+        [Test]
+        public void Circle1Test()
+        {
+            Assert.AreEqual(Area.CircleArea(1), Math.PI);
+        }
+
+        [Test]
+        public void Circle2Test()
+        {
+            Assert.AreEqual(Area.CircleArea(2), Math.PI * 4);
+        }
+
+        [Test]
+        public void CircleArgumentTest()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Area.CircleArea(0));
+        }
+
+        [Test]
+        public void TriangleArgument1Test()
+        {
+            Assert.Throws<ArgumentException>(() => Area.TriangleArea(1, 2));
+        }
+
+        [Test]
+        public void TriangleArgument2Test()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Area.TriangleArea(1, 2, -7));
+        }
+
+        [Test]
+        public void NotTriangle1Test()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Area.TriangleArea(3, 2, 6));
+
+        }
+
+        [Test]
+        public void NotTriangle2Test()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Area.IsRectangular(3, 3, 7));
+
+        }
+
+        [Test]
+        public void TriangleArea1Test()
+        {
+            Assert.AreEqual(Area.TriangleArea(3, 4, 5), 6.0d);
+
+        }
+
+        public void TriangleArea2Test()
+        {
+            Assert.AreEqual(Area.TriangleArea(3, 6, 5) - 7.483315d < 0.000001, true);
+        }
+
+        [Test]
+        public void IsRectangular1Test()
+        {
+            Assert.AreEqual(Area.IsRectangular(3, 4, 5), true);
+        }
+
+        [Test]
+        public void IsRectangular2Test()
+        {
+            Assert.AreEqual(Area.IsRectangular(4, 4, 5), false);
+        }
+    }
+}
---
+++ area/area/Area.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area
+{
+    public class Area
+    {
+        public static double CircleArea(double radius) => radius > 0 ? radius * radius * Math.PI : throw new ArgumentOutOfRangeException();
+
+        public static double TriangleArea(params double[] sides)
+        {
+            if (sides.Length != 3) throw new ArgumentException();
+            double p = 0.0d;
+            foreach (var side in sides)
+                if (side > 0 && side*2<sides.Sum()) p += side; else  throw new ArgumentOutOfRangeException();
+            p /= 2d;
+            double area = p;
+            foreach (var side in sides) area *= (p - side);
+            return Math.Sqrt(area);
+        }
+
+        public static bool IsRectangular(params double[] sides)
+        {
+            if (sides.Length != 3) throw new ArgumentException();
+            foreach (var side in sides) if (side * 2 >= sides.Sum()) throw new ArgumentOutOfRangeException();
+            int h = 0;
+            var cat = new List<int>();
+            for (int i = 1; i < 3; i++)
+            {
+                if (sides[i] <0) throw new ArgumentOutOfRangeException();
+                if (i == 0) continue;
+                if (sides[h] <= sides[i])
+                {
+                    cat.Add(h);
+                    h = i;
+                }
+                else cat.Add(i);
+            }
+            double squareCat = sides[cat[0]] * sides[cat[0]] + sides[cat[1]] * sides[cat[1]];
+            return Math.Abs(Math.Sqrt(squareCat) - sides[h]) < 0.000001;
+        }
+    }
+}
